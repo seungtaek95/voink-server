78ff2be8de395f083dfdd840f479e154448d2f00
seungtaek95 <seungtaek95@gmail.com>
2021-08-09 20:37:56 +0900

    feat: RecordGroup service

--- src/model/record-group/record-group.repository.ts
+++ src/model/record-group/record-group.repository.ts
@@ -2,6 +2,6 @@ import { EntityRepository, Repository } from 'typeorm';
 import { RecordGroup } from './record-group.entity';
 
 @EntityRepository(RecordGroup)
-export class UserRepository extends Repository<RecordGroup> {
-  
+export class RecordGroupRepository extends Repository<RecordGroup> {
+
 }
