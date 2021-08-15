b30ebfa6f16c934051c7385bf9ac4567f99dfc5c
seungtaek95 <seungtaek95@gmail.com>
2021-08-15 16:40:35 +0900

    feat: users seed

---
+++ __test__/seed/user.seed.ts
@@ -0,0 +1,16 @@
+import { CreateUserDto } from '../../src/model/user/user.dto';
+
+const users: CreateUserDto[] = [
+  {
+    provider: 'FACEBOOK',
+    email: 'test1@test.com',
+    name: 'test1',
+  },
+  {
+    provider: 'FACEBOOK',
+    email: 'test1@test.com',
+    name: 'test1',
+  },
+];
+
+export default users;
