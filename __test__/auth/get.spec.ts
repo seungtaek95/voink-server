dbed2e59572966eadf526a592f0eccf803ab92e4
seungtaek95 <seungtaek95@gmail.com>
2021-08-21 15:10:18 +0900

    feat: token이 없는 경우 핸들링 추가

--- __test__/auth/get.spec.ts
+++ __test__/auth/get.spec.ts
@@ -11,6 +11,23 @@ describe('GET auth requests', () => {
   const agent = supertest(app);
 
   describe('GET /auth', () => {
+    test('401 response, Authorization 헤더 없음', (done) => {
+      agent
+        .get('/auth')
+        .expect(401, done);
+    });
+
+    test('401 response, 잘못된 access token', (done) => {
+      // given
+      const token = 'invalid token';
+
+      // when
+      agent
+        .get('/auth')
+        .set('Authorization', token)
+        .expect(401, done);
+    });
+
     test('200 response, 사용자 정보 응답', async () => {
       // given
       const testUser1 = testUsers[0];
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Response } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import { RequestWithUser } from '../interface/request.interface';
 import { AuthService } from '../service/auth.service';
 import { UserService } from '../service/user.service';
 import container from '../utils/container';
+import { HttpError } from '../utils/util';
 
 export function tokenParser() {
   return async (req: RequestWithUser, res: Response, next: NextFunction) => {
@@ -10,7 +12,7 @@ export function tokenParser() {
     const userService = container.get(UserService);
 
     if (!req.headers.authorization) {
-      return next(new Error('No token found'));
+      return next(new HttpError('No token found', 401));
     }
 
     const token = req.headers.authorization.split(' ')[1];
@@ -20,6 +22,9 @@ export function tokenParser() {
       req.user = user;
       next();
     } catch (error) {
+      if (error instanceof JsonWebTokenError) {
+        return next(new HttpError(error.message, 401));
+      }
       next(error);
     }
   };
