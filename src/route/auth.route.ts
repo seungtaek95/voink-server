fd38b4d78b9b9c504c05b0f37694b0d1f5ce0929
seungtaek95 <seungtaek95@gmail.com>
2021-10-24 16:02:49 +0900

    feat: GET /auth시 데이터베이스에서 사용자 조회

--- src/route/auth.route.ts
+++ src/route/auth.route.ts
@@ -2,18 +2,25 @@ import { Response, Router } from 'express';
 import { OAuthRequest, RequestWithUser } from '../interface/request.interface';
 import { tokenParser } from '../middleware/auth.middleware';
 import { AuthService } from '../service/auth.service';
+import { UserService } from '../service/user.service';
 import container from '../utils/container';
-import { wrapAsync } from '../utils/util';
+import { HttpError, wrapAsync } from '../utils/util';
 
 export default function (app: Router) {
   const router = Router();
   const authService = container.get(AuthService);
+  const userService = container.get(UserService);
 
   app.use('/auth', router);
 
   router.get('/',
     tokenParser(),
-    (req: RequestWithUser, res: Response) => {
+    wrapAsync(async (req: RequestWithUser, res: Response) => {
+      const user = await userService.findById(req.user.id);
+      if (!user) {
+        throw new HttpError('Invalid token', 401);
+      }
+
       const now = Math.round(Date.now() / 1000);
       res.status(200).json({
         id: req.user.id,
@@ -21,7 +28,7 @@ export default function (app: Router) {
         expiresIn: req.user.exp - now,
       });
     }
-  );
+  ));
 
   router.post('/login/facebook',
     wrapAsync(async (req: OAuthRequest, res: Response) => {
