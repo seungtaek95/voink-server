ade4b05fd31dfca8908c6a0e1ad513dc46673946
seungtaek95 <seungtaek95@gmail.com>
2021-11-21 14:45:00 +0900

    delete: 필요없는 메소드 삭제

--- src/service/cloud-storage.service.ts
+++ src/service/cloud-storage.service.ts
@@ -1,4 +1,3 @@
-import { Readable } from 'stream';
 import { Bucket } from '@google-cloud/storage';
 import { inject, injectable } from 'inversify';
 import { TYPE } from '../loader/container';
@@ -11,19 +10,9 @@ export class CloudStorageService {
 
   constructor(
     @inject(TYPE.recordBucket) private recordBucket: Bucket,
-    @inject(TYPE.profileBucket) private profileBucket: Bucket,
   ) {}
 
-  uploadUserPicture(source: Readable, userId: number, size: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const dest = this.profileBucket.file(`${userId}/${size}.jpeg`).createWriteStream();
-      source.pipe(dest)
-        .on('finish', resolve)
-        .on('error', reject);
-    });
-  }
-
-  getRecordUploadUrls(userId: string | number, count: number) {
+  getRecordUploadUrls(userId: number, count: number) {
     const getUploadUrlPromises = new Array(count);
     for (let i = 0; i < count; i++) {
       getUploadUrlPromises[i] = this.getRecordUploadUrl(userId);
@@ -31,7 +20,7 @@ export class CloudStorageService {
     return Promise.all(getUploadUrlPromises);
   }
 
-  private async getRecordUploadUrl(userId: string | number) {
+  private async getRecordUploadUrl(userId: number) {
     const key = nanoid();
     const thumbnailPath = `${userId}/${this.tempDirName}/${key}.jpg`;
     const recordPath = `${userId}/${this.tempDirName}/${key}.m4a`;
